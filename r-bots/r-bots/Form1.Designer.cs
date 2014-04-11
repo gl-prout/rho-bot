32d200df859d6f8fa990dba6f609159be744899b
rajoharisonm <ymro.mhery@gmail.com>
2014-04-11 15:06:38 +0300

    Syntheses vocal

--- r-bots/r-bots/Form1.Designer.cs
+++ r-bots/r-bots/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.Camera2 = new System.Windows.Forms.PictureBox();
             this.ListeDesProcessus = new System.Windows.Forms.RichTextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Camera1)).BeginInit();
@@ -123,12 +124,23 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Processus";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(428, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
@@ -161,6 +173,7 @@
         private System.Windows.Forms.PictureBox Camera2;
         private System.Windows.Forms.RichTextBox ListeDesProcessus;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- r-bots/r-bots/Form1.cs
+++ r-bots/r-bots/Form1.cs
@@ -24,6 +24,44 @@ namespace r_bots
         private void Form1_Load(object sender, EventArgs e)
         {
             ListeDesProcessus.Text += "Programme initialisé...\n";
+            
+        }
+
+        private void nouveauMail()
+        {
+            Synthese reponse = new Synthese();
+            ListeDesProcessus.Text += "POP3 Mail Client Demo\n";
+            ListeDesProcessus.Text += "=====================\n\n";
+            try
+            {
+                //prepare pop client
+                // TODO: Replace username and password with your own credentials.
+                Pop3MailClient DemoClient = new Pop3MailClient("pop.gmail.com", 995, true, "projet.rhobots@gmail.com", "projet2014");
+                DemoClient.IsAutoReconnect = true;
+
+                //remove the following line if no tracing is needed
+                DemoClient.Trace += new TraceHandler(Console.WriteLine);
+                DemoClient.ReadTimeout = 60000; //give pop server 60 seconds to answer
+
+                //establish connection
+                DemoClient.Connect();
+
+                //get mailbox statistics
+                int NumberOfMails, MailboxSize;
+                DemoClient.GetMailboxStats(out NumberOfMails, out MailboxSize);
+                ListeDesProcessus.Text += "Données reçu " + NumberOfMails + " nouveaux mails\n";
+                reponse.Parler("Vous avez " + NumberOfMails + " nouveaux mail");
+
+
+                //close connection
+                DemoClient.Disconnect();
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nouveauMail();
         }
     }
 }
